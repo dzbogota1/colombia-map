c92ade2400b2f02058553587bfd67c5212cee9f3
david zornosa <dzbogota1@gmail.com>
2025-09-10 13:29:55 -0500

    export to docs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export"
+      output: 'export',
+  distDir: 'docs',
+  basePath: '/colombia-map',
+  assetPrefix: '/colombia-map/',
 };
 
 export default nextConfig;
